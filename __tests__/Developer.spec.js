9358bc80a812a371cfd85a47cd32392f6f5d13fc
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-05 12:51:58 -0700

    Comment out experimental testing code.  Not working.

--- __tests__/Developer.spec.js
+++ __tests__/Developer.spec.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Enzyme, { shallow, render } from 'enzyme';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
-import Adapter from 'enzyme-adapter-react-16';
-import Developer from '../client/src/components/Developer.jsx';
-import DevStyled from '../client/src/components/Developer.jsx';
-import SpecStyled from '../client/src/components/Developer.jsx';
+// import React from 'react';
+// import Enzyme, { shallow, render } from 'enzyme';
+// import renderer from 'react-test-renderer';
+// import 'jest-styled-components';
+// import Adapter from 'enzyme-adapter-react-16';
+// import Developer from '../client/src/components/Developer.jsx';
+// import DevStyled from '../client/src/components/Developer.jsx';
+// import SpecStyled from '../client/src/components/Developer.jsx';
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 // describe('<Developer />', () => {
 //   it('renders with correct css height, color', () => {
