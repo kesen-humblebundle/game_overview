a3b55d624ddb888d5a3eda39fc1955c402286d19
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-17 16:12:06 -0700

    Update server.js
    change port to env var

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const app = require('./index.js');
 
-app.listen(3002, (err) => {
+app.listen(process.env.OVERVIEW_PORT, (err) => {
   if (err) {
     throw err;
   }
-  console.log('server listening on port: ', 3002);
+  console.log('server listening on port: ', process.env.OVERVIEW_PORT);
 });
