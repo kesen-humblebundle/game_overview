4f3d63cf19a43fc0f0d5ba99b49af6b5cacfc0aa
Chris Hesterman <chrishesterman14@gmail.com>
2020-07-18 08:33:03 -0700

    Update server.js
    adding back 'dotenv' for deploy purposes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env')
+});
 const app = require('./index.js');
 
 app.listen(process.env.OVERVIEW_PORT, (err) => {
